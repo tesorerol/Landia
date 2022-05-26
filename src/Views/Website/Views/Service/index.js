52140dcb3efb4e32e18b2abbd76213f18cad3628
tesorerol <luistesorero5@gmail.com>
2022-05-25 21:20:31 -0500

    fix2

--- src/Views/Website/Views/Service/index.js
+++ src/Views/Website/Views/Service/index.js
@@ -12,37 +12,18 @@ const Service = () => {
                                 <div class="services-icon per-slide">
                                     <i class="lni-cup wow fadeInUp"></i>
                                 </div>
-                                <h1 class="mb-4 text-white per-slide wow fadeInRight font-weight-light shining-text">¿Que es landia?</h1>
+                                <h1 class="mb-4 text-white per-slide wow fadeInRight font-weight-light shining-text">Visión</h1>
                                 <div class="testimonial-line wow fadeInRight"></div>
-                                <p data-aos="fade-left" class="mb-5 per-slide text-white wow fadeInLeft" style={{ textAlign: "justify" }}>Landia reconoce la necesidad de crear y desarrollar nuevas formas de fomento
-                                    económico, a través de prácticas económicas rentables y simbióticas a nivel medioambiental
-                                    y social. En este momento es indispensable poner en práctica sistemas empresariales
-                                    ecológicamente auto-regenerativos1
-                                    , pensados y creados para lograr el mejor rendimiento
-                                    posible con un horizonte autosustentable en el mundo digital y físico. El futuro debe ser por
-                                    antonomasia auto-regenerativo, sino no habrá futuro ni legado ambiental a las siguientes
-                                    generaciones</p>
+                                <p data-aos="fade-left" class="mb-5 per-slide text-white wow fadeInLeft" style={{ textAlign: "justify" }}>La visión de Landia es potenciar, crear, desarrollar e integrar formas de fomento económico que sean simbióticas a nivel medioambiental, social y permitan el desarrollo del espíritu humano y científico. Con esto se busca que los activos naturales que permiten la vida en la tierra, adquieran un valor cuantificable económicamente en el blockchain a través de un marketplace cuya infraestructura base incluye galerías NFT, Token, Turismo Digital, Espacios de Disfrute Físicos y dentro de un Metaverso realista con juegos Play to Earn, así como la conexión e integración de Landia con otros ecosistemas y viceversa. </p>
                             </div>
                             <div class="services-item item text-center text-md-right">
                                 <div class="services-icon per-slide">
                                     <i class="lni-world wow fadeInUp"></i>
                                 </div>
-                                <h1 class="mb-4 text-white per-slide wow fadeInRight font-weight-light shining-text">PROBLEMATIZACIÓN </h1>
+                                <h1 class="mb-4 text-white per-slide wow fadeInRight font-weight-light shining-text">De lo digital a lo físico y de lo físico a lo digital </h1>
                                 <div class="testimonial-line wow fadeInRight"></div>
                                 <p data-aos="fade-left" class="mb-5 per-slide text-white wow fadeInLeft" style={{ textAlign: "justify" }}>
-                                    La pérdida de ecosistemas causará una baja en el GDP de alrededor de $2.7 trillones
-                                    para el 2030, con mayor intensidad para los países de ingresos bajos (Banco Mundial 2021).
-                                    Además, dentro del mundo blockchain los procesos de validación PoW requieren demasiado
-                                    poder energético para funcionar. Landia da una solución real y concisa a este problema global
-                                    antes de que la temperatura global supere los 1,5° C. Utilizando la capacidad tecnológica a
-                                    bajo coste energético interoperable y de consenso del mundo DeFi, dando auge a todo un
-                                    nuevo ecosistema de realidades auto-regenerativas primeramente a través del Token Land
-                                    para financiar proyectos de protección ambiental, investigación científica y espiritual y
-                                    filantropía entrelazadas por corredores biológicos. Comercializando programas empresariales
-                                    e individuales de descarbonización, generando pooles de liquidez, staking, yieldfarming para
-                                    proyectos en incubación, y una vez recaudada la liquidez necesaria también la construcción
-                                    habitacional inteligente CO2 neutral auto sustentable, innovación, entre otras, según se
-                                    pruebe cuales diseños son los más eficaces, eficientes y rentables en la praxis.
+                                    Landia genera una economía que va del mundo físico a lo digital y viceversa, logrando así impactar y generar necesidad en ambas esferas de las 2 realidades y en el punto de encuentro entre ambas: 1. la Realidad Física 2.la realidad del Metaverso Landia 3. Su Conexión con el Resto de Ecosistemas, en aras de una ineludible transición hacia una digitalización. Esta transición conlleva la responsabilidad de transicionar también hacia formas simbióticas y regenerativas en temas medioambientales y sociales.
                                 </p>
                             </div>
                             <div class="services-item item text-center text-md-right">
@@ -51,18 +32,7 @@ const Service = () => {
                                 </div>
                                 <h1 class="mb-4 text-white per-slide wow fadeInRight font-weight-light shining-text">BOSQUES TROPICALES</h1>
                                 <div class="testimonial-line wow fadeInRight"></div>
-                                <p data-aos="fade-left" class="mb-5 per-slide text-white wow fadeInLeft" style={{ textAlign: "justify" }}>
-                                    La necesidad de proteger los bosques y selvas tropicales es uno de los pilares
-                                    máximos para garantizar ambientes globales habitables. Las zonas de flora y fauna tropical
-                                    ocupan menos del 7%, que varían según la definición, de la superficie terrestre de la Tierra,
-                                    pero estas selvas tropicales albergan alrededor de la mitad de las especies de la Tierra y son
-                                    ecológica, económica y culturalmente cruciales para los problemas de seguridad alimentaria
-                                    mundial, cambio climático, biodiversidad y salud humana. (García RA et al. 2009). Además,
-                                    dos tercios de las especies se encuentran en los trópicos (Groombridge y Jenkins 2003), las
-                                    estimaciones actuales son que entre 3,3 y 20 millones de especies residen allí, la inmensa
-                                    mayoría en los bosques tropicales (Lewis, 2006). Si partimos de que las selvas tropicales
-                                    albergan aproximadamente dos tercios de los seres vivientes que existen en la Tierra, con
-                                    altas tasas de endemismo y un gran número de especies raras. </p>
+                                <p data-aos="fade-left" class="mb-5 per-slide text-white wow fadeInLeft" style={{ textAlign: "justify" }}>Landia es una plataforma DeFi cuya misión principal es regenerar 250 millones de hectáreas de bosque en los próximos 14 años sin caer en prácticas monocultivistas, a través de planes de manejo forestal y ambiental premeditados. A través de nuestra economía circular, políticas de gobernanza y consenso a través de nuestro DAO se busca tomar las mejores decisiones en conjunto para generar riqueza y que esta sea redistribuida a través de nuestro ecosistema y así lograr una expansión masiva.</p>
                             </div>
                         </div>
                     </div>
--- src/Views/Website/Views/Team/index.js
+++ src/Views/Website/Views/Team/index.js
@@ -1,34 +1,39 @@
 import React from 'react'
 
 const Team = () => {
-  return (
-    <section class="section bg-team" id="page3">
-    <div class="bg-overlay bg-black opacity-4"></div>
-    <div class="container h-100">
-        <div class="row h-100">
-            <div class="col-md-6 my-auto">
-                <div id="testimonial-quote" class="owl-carousel">
-                    <div class="item">
-                        <div class="testimonial-quote whitecolor">
-                            <div class="team-img mb-4"><img src="/images/testimonial-img1.jpg" alt="img"/></div>
-                            <h1 class="font-weight-light mb-3 alt-font">Arturo Montanaro</h1>
-                            <div class="mb-3 testimonial-line wow fadeInleft"></div>
-                            <div class="mb-1 mb-md-5"> <span>CEO</span></div>
-                            <div class="team-description mb-1 mb-md-5"><p class="m-0">Texto Texto</p></div>
-                        </div>
-                    </div>
+    return (
+        <section class="section bg-team" id="page3">
+            <div class="bg-overlay bg-black opacity-4"></div>
+            <div class="container h-100">
+                <div class="row h-100">
+                    <div class="col-md-6 my-auto">
+                        <div id="testimonial-quote" class="owl-carousel">
+                            <div class="item">
+                                <div class="testimonial-quote whitecolor">
+                                    <div class="team-img mb-4"><img src="/images/testimonial-img1.jpg" alt="img" /></div>
+                                    <h1 class="font-weight-light mb-3 alt-font">Arturo Montanaro</h1>
+                                    <div class="mb-3 testimonial-line wow fadeInleft"></div>
+                                    <div class="mb-1 mb-md-5"> <span>CEO</span></div>
+                                    <div class="team-description mb-1 mb-md-5"><p class="m-0">Texto Texto</p></div>
+                                </div>
+                            </div>
 
-                    <div class="item">
-                        <div class="testimonial-quote whitecolor">
-                            <div class="team-img mb-4"><img src="/images/testimonial-img2.jpg" alt="img"/></div>
-                            <h1 class="font-weight-light mb-3 alt-font">Guillermo</h1>
-                            <div class="mb-3 testimonial-line wow fadeInleft"></div>
-                            <div class="mb-5"> <span>??</span></div>
-                            <div class="team-description mb-5"><p>Texto Texto.</p></div>
-                        </div>
-                    </div>
+                            <div class="item">
+                                <div class="testimonial-quote whitecolor">
+                                    <div class="team-img mb-4"><img src="/images/testimonial-img2.jpg" alt="img" /></div>
+                                    <h1 class="font-weight-light mb-3 alt-font">Guillermo Gharib</h1>
+                                    <div class="mb-3 testimonial-line wow fadeInleft"></div>
+                                    <div class="mb-5"> <span>COO</span></div>
+                                    <div class="team-description mb-5"><p>+15 Años en Posiciones Gerenciales gestionando equipos de 50 a +250 empleados.<br />
+                                        +10 Años de experiencia en finanzas.<br />
+                                        +7 años de experiencia en Biz Dev<br />
+                                        Customer Engagement & Sales Experience en América del Norte, América del Sur y mercados CAM.<br />
+                                        Seguimiento comprobado de éxito en la gestión de campañas u organizaciones de las que he formado parte.<br />
+                                        Certificación en PMP, Six Sigma, ITIL, manejo de la ira, manejo del tiempo, habilidades blandas, BI y ciencia de datos.</p></div>
+                                </div>
+                            </div>
 
-                    {/* <div class="item">
+                            {/* <div class="item">
                         <div class="testimonial-quote whitecolor">
                             <div class="team-img mb-4"><img src="/images/testimonial-img3.jpg" alt="img"/></div>
                             <h1 class="font-weight-light mb-3 alt-font">Tena Walkin</h1>
@@ -50,19 +55,19 @@ const Team = () => {
                                 quos dolores et quas.</p></div>
                         </div>
                     </div> */}
+                        </div>
+                        <div id="owl-thumbs" class="owl-dots">
+                            <button class="owl-dot active link"><img src="/images/testimonial-img1.jpg" alt="img" /></button>
+                            <button class="owl-dot link"><img src="/images/testimonial-img2.jpg" alt="img" /></button>
+                            <button class="owl-dot link"><img src="/images/testimonial-img3.jpg" alt="img" /></button>
+                            <button class="owl-dot link"><img src="/images/testimonial-img4.jpg" alt="img" /></button>
+                        </div>
+                        <div class="team-item d-none"></div>
+                    </div>
                 </div>
-                <div id="owl-thumbs" class="owl-dots">
-                    <button class="owl-dot active link"><img src="/images/testimonial-img1.jpg"  alt="img"/></button>
-                    <button class="owl-dot link"><img src="/images/testimonial-img2.jpg"  alt="img"/></button>
-                    <button class="owl-dot link"><img src="/images/testimonial-img3.jpg"  alt="img"/></button>
-                    <button class="owl-dot link"><img src="/images/testimonial-img4.jpg"  alt="img"/></button>
-                </div>
-                <div class="team-item d-none"></div>
             </div>
-        </div>
-    </div>
-</section>
-  )
+        </section>
+    )
 }
 
 export default Team
